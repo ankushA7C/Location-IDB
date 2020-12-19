c88215ae1e6e895d984d249752f56194979693df
Ankush Chourasia <ankushchourasia@hotmail.com>
2020-12-19 20:07:01 +0530

    Updates

---
+++ precache-manifest.372580c9e6f4956a475803466cbae091.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90b7d74159b2b8b6fd024360022beb2f",
+    "url": "/Location-IDB/index.html"
+  },
+  {
+    "revision": "8a5ec8bfff16f1e301ee",
+    "url": "/Location-IDB/static/css/main.d85a0958.chunk.css"
+  },
+  {
+    "revision": "7bcc679ead798ae2897c",
+    "url": "/Location-IDB/static/js/2.a4f58557.chunk.js"
+  },
+  {
+    "revision": "253d0d72fd1c6eb1e9606c0cb98dca11",
+    "url": "/Location-IDB/static/js/2.a4f58557.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a5ec8bfff16f1e301ee",
+    "url": "/Location-IDB/static/js/main.304ee489.chunk.js"
+  },
+  {
+    "revision": "edc20e32558d94fb0158",
+    "url": "/Location-IDB/static/js/runtime-main.6decf3f6.js"
+  },
+  {
+    "revision": "e6669fd467c8b7a3674e7553700e0995",
+    "url": "/Location-IDB/static/media/noLocation.e6669fd4.png"
+  }
+]);
